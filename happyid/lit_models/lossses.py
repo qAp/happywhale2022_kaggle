dae748e7829e42997204290ba32942df44589af9
Jack Yu <jackchungchiehyu@gmail.com>
2022-02-07 22:14:26 +0800

    Euclidean distance calculation function

---
+++ happyid/lit_models/lossses.py
@@ -0,0 +1,20 @@
+
+import torch
+
+
+
+def euclidean_dist(x, y):
+    """
+    Args:
+      x: pytorch Variable, with shape [m, d]
+      y: pytorch Variable, with shape [n, d]
+    Returns:
+      dist: pytorch Variable, with shape [m, n]
+    """
+    m, n = x.size(0), y.size(0)
+    xx = torch.pow(x, 2).sum(1, keepdim=True).expand(m, n)
+    yy = torch.pow(y, 2).sum(1, keepdim=True).expand(n, m).t()
+    dist = xx + yy
+    dist.addmm_(1, -2, x, y.t())
+    dist = dist.clamp(min=1e-12).sqrt()  # for numerical stability
+    return dist
