944b680dcc81b46c6d9d1ec9c686673ae2c32609
Jack Yu <jackchungchiehyu@gmail.com>
2022-02-08 20:32:44 +0800

    Vector normalization function

--- happyid/lit_models/losses.py
+++ happyid/lit_models/losses.py
@@ -2,6 +2,16 @@
 import torch
 
 
+def normalize(x, axis=-1):
+    """Normalizing to unit length along the specified dimension.
+    Args:
+      x: pytorch Variable
+    Returns:
+      x: pytorch Variable, same shape as input
+    """
+    x = 1. * x / (torch.norm(x, 2, axis, keepdim=True).expand_as(x) + 1e-12)
+    return x
+
 
 def euclidean_dist(x, y):
     """
