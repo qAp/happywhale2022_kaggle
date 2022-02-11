3a651a7cfc68c55665072f406542b871f8c310ff
Jack Yu <jackchungchiehyu@gmail.com>
2022-02-11 22:01:01 +0800

    Save Imagenet's mean and standard deviation

--- happyid/data/config.py
+++ happyid/data/config.py
@@ -1,7 +1,13 @@
 
+import numpy as np
+
+
 
 DIR_BASE = '/kaggle/input/happy-whale-and-dolphin'
 
 NUM_INDIVIDUALS = 15587
+
+MEAN_IMG = np.array([0.485, 0.456, 0.406])
+STD_IMG = np.array([0.229, 0.224, 0.225])
 
 
