5e3fba4f27b76efb18c98299c74c343aa7af03a4
Jack Yu <jackchungchiehyu@gmail.com>
2022-02-23 23:24:01 +0800

    Set Imagenet constants as type float32

--- happyid/data/config.py
+++ happyid/data/config.py
@@ -7,7 +7,7 @@ DIR_BASE = '/kaggle/input/happy-whale-and-dolphin'
 
 NUM_INDIVIDUALS = 15587
 
-MEAN_IMG = np.array([0.485, 0.456, 0.406])
-STD_IMG = np.array([0.229, 0.224, 0.225])
+MEAN_IMG = np.array([0.485, 0.456, 0.406], dtype='float32')
+STD_IMG = np.array([0.229, 0.224, 0.225], dtype='float32')
 
 
