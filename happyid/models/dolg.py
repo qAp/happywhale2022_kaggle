ba9428c23d1d8902a33697969308799976a1cd1a
Jack Yu <jackchungchiehyu@gmail.com>
2022-03-06 01:21:47 +0800

    Components of DOLG-EfficientNet
    Import the component modules that make up Christof Henkel's
    DOLG-EfficientNet (for the 2021 Google Landmark Challenge).

---
+++ happyid/models/dolg.py
@@ -0,0 +1,161 @@
+
+'''
+DOLG EfficientNet by Christof Henkel.
+https://github.com/ChristofHenkel/kaggle-landmark-2021-1st-place/blob/main/models/ch_mdl_dolg_efficientnet.py
+'''
+
+import timm
+from torch import nn
+import torch
+import math
+from torch.nn import functional as F
+from torch.nn.parameter import Parameter
+import numpy as np
+
+
+class Swish(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, i):
+        result = i * torch.sigmoid(i)
+        ctx.save_for_backward(i)
+        return result
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        i = ctx.saved_variables[0]
+        sigmoid_i = torch.sigmoid(i)
+        return grad_output * (sigmoid_i * (1 + i * (1 - sigmoid_i)))
+
+
+class Swish_module(nn.Module):
+    def forward(self, x):
+        return Swish.apply(x)
+
+
+class DenseCrossEntropy(nn.Module):
+    def forward(self, x, target):
+        x = x.float()
+        target = target.float()
+        logprobs = torch.nn.functional.log_softmax(x, dim=-1)
+
+        loss = -logprobs * target
+        loss = loss.sum(-1)
+        return loss.mean()
+
+
+class ArcMarginProduct_subcenter(nn.Module):
+    def __init__(self, in_features, out_features, k=3):
+        super().__init__()
+        self.weight = nn.Parameter(
+            torch.FloatTensor(out_features*k, in_features))
+        self.reset_parameters()
+        self.k = k
+        self.out_features = out_features
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+
+    def forward(self, features):
+        cosine_all = F.linear(F.normalize(features), F.normalize(self.weight))
+        cosine_all = cosine_all.view(-1, self.out_features, self.k)
+        cosine, _ = torch.max(cosine_all, dim=2)
+        return cosine
+
+
+def gem(x, p=3, eps=1e-6):
+    return F.avg_pool2d(x.clamp(min=eps).pow(p), (x.size(-2), x.size(-1))).pow(1./p)
+
+
+class GeM(nn.Module):
+    def __init__(self, p=3, eps=1e-6, p_trainable=False):
+        super(GeM, self).__init__()
+        if p_trainable:
+            self.p = Parameter(torch.ones(1)*p)
+        else:
+            self.p = p
+        self.eps = eps
+
+    def forward(self, x):
+        ret = gem(x, p=self.p, eps=self.eps)
+        return ret
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(' + 'p=' + '{:.4f}'.format(self.p.data.tolist()[0]) + ', ' + 'eps=' + str(self.eps) + ')'
+
+
+class MultiAtrousModule(nn.Module):
+    def __init__(self, in_chans, out_chans, dilations):
+        super(MultiAtrousModule, self).__init__()
+
+        self.d0 = nn.Conv2d(in_chans, 512, kernel_size=3,
+                            dilation=dilations[0], padding='same')
+        self.d1 = nn.Conv2d(in_chans, 512, kernel_size=3,
+                            dilation=dilations[1], padding='same')
+        self.d2 = nn.Conv2d(in_chans, 512, kernel_size=3,
+                            dilation=dilations[2], padding='same')
+        self.conv1 = nn.Conv2d(512 * 3, out_chans, kernel_size=1)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+
+        x0 = self.d0(x)
+        x1 = self.d1(x)
+        x2 = self.d2(x)
+        x = torch.cat([x0, x1, x2], dim=1)
+        x = self.conv1(x)
+        x = self.relu(x)
+        return x
+
+
+class SpatialAttention2d(nn.Module):
+    def __init__(self, in_c):
+        super(SpatialAttention2d, self).__init__()
+        self.conv1 = nn.Conv2d(in_c, 1024, 1, 1)
+        self.bn = nn.BatchNorm2d(1024)
+        self.act1 = nn.ReLU()
+        self.conv2 = nn.Conv2d(1024, 1, 1, 1)
+        # use default setting.
+        self.softplus = nn.Softplus(beta=1, threshold=20)
+
+    def forward(self, x):
+        '''
+        x : spatial feature map. (b x c x w x h)
+        att : softplus attention score 
+        '''
+        x = self.conv1(x)
+        x = self.bn(x)
+
+        feature_map_norm = F.normalize(x, p=2, dim=1)
+
+        x = self.act1(x)
+        x = self.conv2(x)
+        att_score = self.softplus(x)
+        att = att_score.expand_as(feature_map_norm)
+
+        x = att * feature_map_norm
+        return x, att_score
+
+
+class OrthogonalFusion(nn.Module):
+    def __init__(self):
+        super(OrthogonalFusion, self).__init__()
+
+    def forward(self, fl, fg):
+
+        bs, c, w, h = fl.shape
+
+        fl_dot_fg = torch.bmm(fg[:, None, :], fl.reshape(bs, c, -1))
+        fl_dot_fg = fl_dot_fg.reshape(bs, 1, w, h)
+        fg_norm = torch.norm(fg, dim=1)
+
+        fl_proj = (fl_dot_fg / fg_norm[:, None,
+                   None, None]) * fg[:, :, None, None]
+        fl_orth = fl - fl_proj
+
+        f_fused = torch.cat(
+            [fl_orth, fg[:, :, None, None].repeat(1, 1, w, h)], dim=1)
+        return f_fused
+
+
