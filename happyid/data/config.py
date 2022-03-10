8639ebd224318d3ea5c95af57e7d7055ee13c577
Jack Yu <jackchungchiehyu@gmail.com>
2022-03-10 22:33:43 +0800

    Load individual ID encoder in data.config

--- happyid/data/config.py
+++ happyid/data/config.py
@@ -1,4 +1,4 @@
-
+import joblib
 import numpy as np
 
 
@@ -9,5 +9,7 @@ NUM_INDIVIDUALS = 15587
 
 MEAN_IMG = np.array([0.485, 0.456, 0.406], dtype='float32')
 STD_IMG = np.array([0.229, 0.224, 0.225], dtype='float32')
+
+ID_ENCODER = joblib.load('/kaggle/input/happyid-label-encoding/label_encoder')
 
 
