23ee230eaae2b50af281dbea12021a8bf6a3da93
Jack Yu <jackchungchiehyu@gmail.com>
2022-03-11 23:14:36 +0800

    Set early stopping patience to 100

--- training/run_experiment.py
+++ training/run_experiment.py
@@ -43,7 +43,7 @@ def main():
         logger.log_hyperparams(vars(args))
 
     early_stopping_callback = pl.callbacks.EarlyStopping(
-        monitor='valid_loss', mode='min', patience=10)
+        monitor='valid_loss', mode='min', patience=100)
 
     model_checkpoint_callback = pl.callbacks.ModelCheckpoint(
         filename=(f'fold{args.fold:d}-' +
