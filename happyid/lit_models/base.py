6c9dfbd0ccf999371d824530c5995df4e3728889
Jack Yu <jackchungchiehyu@gmail.com>
2022-03-21 16:09:40 +0800

    Revert "Print xb for debugging"
    This reverts commit 91d2694e68d3e29801babdd68a311a2b5ee61131.

--- happyid/lit_models/base.py
+++ happyid/lit_models/base.py
@@ -92,7 +92,6 @@ class BaseLitModel(pl.LightningModule):
     @torch.no_grad()
     def predict_step(self, batch, batch_idx, dataloader_idx=0):
         xb, *_ = batch
-        print(xb)
         xb = xb.permute(0, 3, 1, 2)
         return self.model(xb, return_emb=self.return_emb)
 
