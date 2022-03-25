8b2ea66161aff70640818f73aa5de9c1b68a563c
Jack Yu <jackchungchiehyu@gmail.com>
2022-03-25 19:34:19 +0800

    Average all new_individual embs for retrieval

--- happyid/retrieval_cv.py
+++ happyid/retrieval_cv.py
@@ -19,16 +19,50 @@ from happyid.retrieval import retrieval_predict
 def _setup_parser():
     parser = setup_parser()
     _add = parser.add_argument
+
     _add('--folds_knownid_emb_path', nargs='+', type=str, 
          default=5*['emb.npz'])
     _add('--folds_knownid_emb_meta_path', nargs='+', type=str,
          default=5*['train.csv'])
+
+    _add('--folds_newid_emb_path', nargs='+', type=str,
+         default=5 * [None])
+    _add('--folds_newid_emb_meta_path', nargs='+', type=str,
+         default=5 * [None])
+
     _add('--folds_model_class', nargs='+', type=str, default=5*['DOLG'])
     _add('--folds_backbone_name', nargs='+', type=str, default=5*['resnet18'])
     _add('--folds_checkpoint_path', nargs='+', type=str,
         default=5*['best.pth'], help='Model checkpoint paths for the 5 folds.')
 
     return parser
+
+
+def _get_ref_emb(args):
+    ifold = args.fold
+    knownid_emb_path = args.folds_knownid_emb_path[ifold]
+    knownid_emb_meta_path = args.folds_knownid_emb_meta_path[ifold]
+    newid_emb_path = args.folds_newid_emb_path[ifold]
+    newid_emb_meta_path = args.folds_newid_emb_meta_path[ifold]
+
+    knownid_emb = np.load(knownid_emb_path)['embed']
+    knownid_emb_meta = pd.read_csv(knownid_emb_meta_path)
+
+    if newid_emb_path is not None:
+
+        newid_emb = np.load(newid_emb_path)['embed']
+        ref_emb = np.concatenate(
+            [knownid_emb, newid_emb.mean(axis=0, keepdims=True)],
+            axis=0)
+
+        newid_df = pd.DataFrame(columns=knownid_emb_meta.columns)
+        newid_df['individual_id'] = ['new_individual']
+        ref_emb_meta = knownid_emb_meta.append(newid_df, ignore_index=True)
+    else:
+        ref_emb = knownid_emb
+        ref_emb_meta = knownid_emb_meta
+
+    return ref_emb, ref_emb_meta
 
 
 def main():
@@ -38,6 +72,8 @@ def main():
     assert (NUM_FOLD
             == len(args.folds_knownid_emb_path)
             == len(args.folds_knownid_emb_meta_path)
+            == len(args.folds_newid_emb_path)
+            == len(args.folds_newid_emb_meta_path)
             == len(args.folds_model_class)
             == len(args.folds_backbone_name)
             == len(args.folds_checkpoint_path)
@@ -53,9 +89,7 @@ def main():
         args.backbone_name = args.folds_backbone_name[ifold]
         args.load_from_checkpoint = args.folds_checkpoint_path[ifold]
 
-        emb_df = pd.read_csv(args.folds_knownid_emb_meta_path[ifold])
-        loaded = np.load(args.folds_knownid_emb_path[ifold])
-        ref_emb = torch.from_numpy(loaded['embed'])
+        ref_emb, emb_df = _get_ref_emb(args)
 
         data_class = import_class(f'happyid.data.{args.data_class}')
         model_class = import_class(f'happyid.models.{args.model_class}')
