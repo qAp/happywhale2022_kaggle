8d79e74d25d7d31a9ec11b65ab94f9e13625f7e4
Jack Yu <jackchungchiehyu@gmail.com>
2022-04-04 18:00:45 +0800

    Change number of folds at CV to 1

--- happyid/data/config.py
+++ happyid/data/config.py
@@ -8,6 +8,6 @@ DIR_BASE = '/kaggle/input/happy-whale-and-dolphin'
 MEAN_IMG = np.array([0.485, 0.456, 0.406], dtype='float32')
 STD_IMG = np.array([0.229, 0.224, 0.225], dtype='float32')
 
-NUM_FOLD = 5
+NUM_FOLD = 1
 
 
