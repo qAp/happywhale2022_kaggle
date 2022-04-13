b68d74ac720b1287a7627dab786996862db9ffd1
Jack Yu <jackchungchiehyu@gmail.com>
2022-04-13 17:02:51 +0800

    Blending option for submission

--- inference/retrieval_submission.py
+++ inference/retrieval_submission.py
@@ -16,9 +16,10 @@ import matplotlib.pyplot as plt
 from happyid.data.config import *
 from happyid.utils import import_class, setup_parser
 from happyid.lit_models.losses import euclidean_dist
-from happyid.retrieval import (load_embedding, load_ref_test_dfs, 
-                               get_emb_subset, retrieve_topk, 
-                               get_closest_ids_df, predict_top5)
+from happyid.retrieval import (
+    load_embedding, load_fliplr_df, load_ref_test_dfs, 
+    get_emb_subset, retrieve_topk, 
+    get_closest_ids_df, predict_top5)
 
 
 def _setup_parser():
@@ -26,6 +27,7 @@ def _setup_parser():
     _add = parser.add_argument
     _add('--emb_dir', type=str, default='/kaggle/input/happyid-tvet-data')
     _add('--retrieval_crit', type=str, default='cossim')
+    _add('--blending', type=ast.literal_eval, default='False')
     _add('--newid_close_thres', type=float, default=.8,
          help='new_individual distance threshold.')
     return parser
@@ -45,13 +47,23 @@ def main():
         test_splits=None)
     test_df = pd.read_csv(
         '/kaggle/input/happy-whale-and-dolphin/sample_submission.csv')
-
     ref_df, ref_emb = get_emb_subset(emb_df, emb, ref_df)
     test_df, test_emb = get_emb_subset(emb_df, emb, test_df)
 
-    topked = retrieve_topk(test_emb, ref_emb, k=50, 
-                           batch_size=args.batch_size,
-                           retrieval_crit=args.retrieval_crit)
+    if args.blending:
+        new_df = load_fliplr_df()
+        new_df, new_emb = get_emb_subset(emb_df, emb, new_df)
+
+        topked = retrieve_topk(ref_emb, new_emb, k=3,
+                               batch_size=20_000,
+                               retrieval_crit=args.retrieval_crit)
+        ref_to_new_close = torch.mean(topked['values'], dim=1)
+
+    topked = retrieve_topk(
+        test_emb, ref_emb, k=50, 
+        batch_size=args.batch_size,
+        retrieval_crit=args.retrieval_crit,
+        ref_to_new_close=ref_to_new_close if args.blending else None)
 
     close_df = get_closest_ids_df(test_df, ref_df, topked, 
                                   retrieval_crit=args.retrieval_crit)
